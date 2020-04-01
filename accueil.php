3a72a4b206843ae3fd0cef23f72e4a204739b673
Zoumazette <isabelle.libessart@laposte.net>
2020-04-01 23:31:47 +0200

    Modification index, accueil php, achat php et création book.html

--- accueil.php
+++ accueil.php
@@ -1,18 +1,27 @@
-<?php
-//var_dump(_POST);
-$name =  $_POST['username'];
-$keypass = $_POST['password'];
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-//Se connecter à la base de données
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=AWOF','stagiaire','stagiaire');
-} catch (PDOException $e) {
-    echo 'connexion échouée';
-}
-
-//Insérer la ligne utilisateur en bdd
-$sql = "INSERT INTO user (name_user, password_user) VALUES (?,?)";
-$preparation = $pdo->prepare($sql);
-$preparation->bindParam(1,$name);
-$preparation->bindParam(2,$keypass);
-$preparation->execute();
+<form>
+    <div class="form-row align-items-center">
+        <div class="col-sm-3 my-1">
+        <label class="sr-only" for="inlineFormInputName">Identifiant</label>
+        <input type="text" class="form-control" id="inlineFormInputName" placeholder="Pseudo">
+        </div>
+        <div class="col-sm-3 my-1">
+        <label class="sr-only" for="inlineFormInputGroupUsername">Mot de passe</label>
+        <div class="input-group">
+        <input type="password" class="form-control" id="inlineFormInputGroupUsername" placeholder="Password">
+        </div>
+        </div>
+        <div class="col-auto my-1">
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="autoSizingCheck2">
+            <label class="form-check-label" for="autoSizingCheck2">
+            Se souvenir de moi
+            </label>
+        </div>
+        </div>
+        <div class="col-auto my-1">
+        <button type="submit" class="btn btn-primary">Envoyer</button>
+        </div>
+    </div>
+</form>
--- achat.php
+++ achat.php
@@ -1,1 +1,21 @@
 <!--accessible uniquement aux loggés (vérification  pseudo/mot de passe-->
+<?php
+//var_dump(_POST);
+$username =  $_POST['username'];
+$email = $_POST['email'];
+$keypass = $_POST['keypass'];
+
+//Se connecter à la base de données
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=atelier_prairie','stagiaire','stagiaire');
+} catch (PDOException $e) {
+    echo 'connexion échouée';
+}
+
+//Insérer la ligne utilisateur en bdd
+$sql = "INSERT INTO user (user_name, user_email, user_keypass) VALUES (?,?,?)";
+$preparation = $pdo->prepare($sql);
+$preparation->bindParam(1,$username);
+$preparation->bindParam(2,$email);
+$preparation->bindParam(3,$keypass);
+$preparation->execute();
