456eef2fbb525537691f2191e17ed2428200e04f
Zoumazette <isabelle.libessart@laposte.net>
2020-04-02 13:20:50 +0200

    Mise à jour des formulaires accueil et achat

--- accueil.php
+++ accueil.php
@@ -1,27 +1,25 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 <form>
-    <div class="form-row align-items-center">
-        <div class="col-sm-3 my-1">
-        <label class="sr-only" for="inlineFormInputName">Identifiant</label>
-        <input type="text" class="form-control" id="inlineFormInputName" placeholder="Pseudo">
+    <div class="form-row">
+        <div class="form-group col-md-6">
+        <label for="inputEmail4">Pseudo/Login</label>
+        <input type="text" class="form-control" id="user_name">
         </div>
-        <div class="col-sm-3 my-1">
-        <label class="sr-only" for="inlineFormInputGroupUsername">Mot de passe</label>
-        <div class="input-group">
-        <input type="password" class="form-control" id="inlineFormInputGroupUsername" placeholder="Password">
-        </div>
-        </div>
-        <div class="col-auto my-1">
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="autoSizingCheck2">
-            <label class="form-check-label" for="autoSizingCheck2">
-            Se souvenir de moi
-            </label>
-        </div>
-        </div>
-        <div class="col-auto my-1">
-        <button type="submit" class="btn btn-primary">Envoyer</button>
+        <div class="form-group col-md-6">
+        <label for="inputPassword4">Mot de passe/Password</label>
+        <input type="password" class="form-control" id="user_password">
         </div>
     </div>
+    
+    <div class="form-group">
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="gridCheck">
+        <label class="form-check-label" for="gridCheck">
+            Se souvenir de moi/Check me out
+        </label>
+        </div>
+    </div>
+    
+    <button type="submit" class="btn btn-primary">Envoyer/Sign in</button>
 </form>
--- achat.php
+++ achat.php
@@ -1,4 +1,46 @@
 <!--accessible uniquement aux loggés (vérification  pseudo/mot de passe-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<form>
+    <div class="form-row">
+        <div class="form-group col-md-6">
+        <label for="inputEmail4">Pseudo/Login</label>
+        <input type="text" class="form-control" id="user_name">
+        </div>
+        <div class="form-group col-md-6">
+        <label for="inputPassword4">Mot de passe/Password</label>
+        <input type="password" class="form-control" id="user_password">
+        </div>
+    </div>
+    
+    <div class="form-group">
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="gridCheck">
+        <label class="form-check-label" for="gridCheck">
+            Se souvenir de moi/Check me out
+        </label>
+        </div>
+    </div>
+    
+    <button type="submit" class="btn btn-primary">Envoyer/Sign in</button>
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php
 //var_dump(_POST);
 $username =  $_POST['username'];
