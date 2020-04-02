d31c225a137c9d4ce1709bbd95147226243be461
Zoumazette <isabelle.libessart@laposte.net>
2020-04-02 14:40:34 +0200

    Modif achat.php

--- achat.php
+++ achat.php
@@ -15,34 +15,32 @@
     
     <div class="form-group">
         <div class="form-check">
-        <input class="form-check-input" type="checkbox" id="gridCheck">
+        <input class="form-check-input" type="checkbox" id="keypass">
         <label class="form-check-label" for="gridCheck">
             Se souvenir de moi/Check me out
         </label>
         </div>
     </div>
     
-    <button type="submit" class="btn btn-primary">Envoyer/Sign in</button>
+    <button type="POST" class="btn btn-primary">Envoyer/Sign in</button>
 </form>
 
 
 <?php
-//var_dump(_POST);
-$username =  $_POST['username'];
-$email = $_POST['email'];
+//var_dump(_$POST);
+$username =  $_POST['user_name'];
 $keypass = $_POST['keypass'];
 
 //Se connecter à la base de données
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=atelier_prairie','stagiaire','stagiaire');
+    $pdo = new PDO('mysql:host=localhost;dbname=AWOF','stagiaire','stagiaire');
 } catch (PDOException $e) {
     echo 'connexion échouée';
 }
 
 //Insérer la ligne utilisateur en bdd
-$sql = "INSERT INTO user (user_name, user_email, user_keypass) VALUES (?,?,?)";
+$sql = "INSERT INTO user (name_user, password_user) VALUES (?,?)";
 $preparation = $pdo->prepare($sql);
 $preparation->bindParam(1,$username);
-$preparation->bindParam(2,$email);
-$preparation->bindParam(3,$keypass);
+$preparation->bindParam(2,$keypass);
 $preparation->execute();
